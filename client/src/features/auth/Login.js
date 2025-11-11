3505f0b2a0ee9603fe5f1ddfb3146d5a9b8eef8f
miri6069 <r0534164095@gmail.com>
2025-11-12 01:18:48 +0200

    change login by racheli

--- client/src/features/auth/Login.js
+++ client/src/features/auth/Login.js
@@ -32,7 +32,7 @@ const Login = () => {
     }
     return <div className="app">
         <form onSubmit={handleSubmit}>
-            <h2>Login</h2>
+            <h2>Login to my website -racheli</h2>
             {/* {isError && JSON.stringify(error)}  */}
             {isError && error && (
                 <div className="error-message">
